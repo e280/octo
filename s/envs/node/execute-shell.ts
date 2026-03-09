52c074799add91225108fc0c06ad52f54fab25ff
Chase Moskal <chasemoskal@gmail.com>
2026-03-09 01:22:22 -0700

    fix: colors and .bin path

--- s/envs/node/execute-shell.ts
+++ s/envs/node/execute-shell.ts
@@ -1,10 +1,20 @@
 
 import Stream from "node:stream"
+import {resolve} from "node:path"
 import {spawn} from "node:child_process"
 import {ExecuteShellFn, ExitCode, ProcExternal} from "../../types.js"
 
 export const executeShellNode: ExecuteShellFn = command => {
-	const child = spawn(command, {shell: true})
+	const child = spawn(command, {
+		shell: true,
+		env: {
+			...process.env,
+			PATH: `${resolve("node_modules/.bin")}:${process.env.PATH}`,
+			FORCE_COLOR: "1",
+			CLICOLOR_FORCE: "1",
+			TERM: process.env.TERM ?? "xterm-256color",
+		},
+	})
 
 	return {
 		command,
